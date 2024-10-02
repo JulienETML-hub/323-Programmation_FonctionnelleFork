62244f74cfcceeaa3ca86957a732d4459fa4a588
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-10-02 12:10:38 +0200

    utilsir linq

--- personel/fileCount/Program.cs
+++ personel/fileCount/Program.cs
@@ -1,4 +1,6 @@
-﻿DirectoryInfo directory = new DirectoryInfo(@"C:\temp");
+﻿using System.Linq;
+
+DirectoryInfo directory = new DirectoryInfo(@"C:\temp");
 
 int fileCount;
 
@@ -9,19 +11,24 @@ static List<int> countAllElements(DirectoryInfo directory)
 {
     int fileCount = 0;
     int directoriesCount = 0;
-    List<int> allElements = new List<int>();
-
-    Array dirList = directory.GetDirectories();
 
     fileCount += directory.GetFiles().Count();
     directoriesCount += directory.GetDirectories().Count();
 
-    foreach (DirectoryInfo item in dirList)
+
+    //Tuple aa = dirList.SelectMany (e =>Tuple(filea: e.GetFiles().Count(), directorya: e.GetDirectories().Count())).ToList();
+    List<int> allElements = new List<int>();
+
+    List<DirectoryInfo> dirList = directory.GetDirectories().ToList();
+
+    // allElements[0] += dirList.Aggregate(e => countAllElements(e)[0]);
+    dirList.ForEach(item =>
     {
         fileCount += countAllElements(item)[0];
         directoriesCount += countAllElements(item)[1];
 
     }
+    );
     allElements.Add(fileCount);
     allElements.Add(directoriesCount);
     return allElements;
@@ -29,5 +36,5 @@ static List<int> countAllElements(DirectoryInfo directory)
 
 List<int> fileAndDirectoriesCount = countAllElements(directory);
 
-Console.WriteLine("Fichiers :" + fileAndDirectoriesCount[0]+ "\n Dossiers :" + fileAndDirectoriesCount[1]);
+Console.WriteLine("Fichiers :" + fileAndDirectoriesCount[0]+ " Dossiers :" + fileAndDirectoriesCount[1]);
 Console.ReadLine();
