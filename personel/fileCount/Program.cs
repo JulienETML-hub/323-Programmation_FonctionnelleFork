7433cbf42d2a831a209c8b37cdaf2ecce089f02c
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-10-02 10:21:30 +0200

    feat count file and directory séparement

--- personel/fileCount/Program.cs
+++ personel/fileCount/Program.cs
@@ -3,6 +3,31 @@
 int fileCount;
 
 fileCount = directory.GetFiles().Count();
+int directoryCount = directory.GetDirectories().Count();  
+// Fonction récursive
+static List<int> countAllElements(DirectoryInfo directory)
+{
+    int fileCount = 0;
+    int directoriesCount = 0;
+    List<int> allElements = new List<int>();
 
-Console.WriteLine(fileCount);
+    Array dirList = directory.GetDirectories();
+
+    fileCount += directory.GetFiles().Count();
+    directoriesCount += directory.GetDirectories().Count();
+
+    foreach (DirectoryInfo item in dirList)
+    {
+        fileCount += countAllElements(item)[0];
+        directoriesCount += countAllElements(item)[1];
+
+    }
+    allElements.Add(fileCount);
+    allElements.Add(directoriesCount);
+    return allElements;
+}
+
+List<int> fileAndDirectoriesCount = countAllElements(directory);
+
+Console.WriteLine("Fichiers :" + fileAndDirectoriesCount[0]+ "\n Dossiers :" + fileAndDirectoriesCount[1]);
 Console.ReadLine();
